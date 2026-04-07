57e4326277b25c6e62f08146dc50962683a91122
SPinkBrother <spinkbrotherofficial@gmail.com>
2026-04-06 23:13:21 -0400

    Task 3

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -9,23 +9,32 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
- 
+    return cart.reduce((total , item) => total + item.quantity* 
+parseFloat(item.cost.substring('$', '')), 0 );
   };
 
   const handleContinueShopping = (e) => {
-   
+    onContinueShopping(e);
   };
 
-
+  const handleCheckoutShopping = (e) => {
+    // TODO: 
+  }
 
   const handleIncrement = (item) => {
+    dispatch(updateQuantity({name : item.name, quantity : item.quantity + 1}));
   };
 
   const handleDecrement = (item) => {
-   
+    if (item.quantity > 1){
+        dispatch(updateQuantity({name: item.name, quantity: item.quantity - 1}));
+    } else {
+        dispatch(removeItem(item.name));
+    }
   };
 
   const handleRemove = (item) => {
+    dispatch(removeItem(item.name))
   };
 
   // Calculate total cost based on quantity for an item
